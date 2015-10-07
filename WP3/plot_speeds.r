c537b977c78e53ec57356c41e890193c4ffd22b4
Jason Byrne <jbyrne6@gmail.com>
2015-10-07 17:54:31 +0100

    Added boxplots and histograms

--- WP3/plot_speeds.r
+++ WP3/plot_speeds.r
@@ -42,37 +42,97 @@ names(wp3) <- c("ID",
 
 # Simple plots
 
-#dev.copy("CME_speeds.png",width=8,height=8,unit="in",res=300)
-pdf("CME_Speeds_FP_SSE.pdf",width=8,height=3)
-par(mfrow=c(1,3),pty="s")
-plot(wp3$FP_Speed, wp3$SSE_Speed, 
-	xlab="FP Speed (km/s)", ylab="SSE Speed (km/s)",#main="WP3 CME Speeds",
-	xlim=c(0,3000), ylim=c(0,3000),
-	pch=3)
-abline(0,1,col="black",lty=5)
-reg <- lm(wp3$SSE_Speed~wp3$FP_Speed)
-abline(reg,col="red")
-legend("bottomright",c(paste("y =",signif(reg$coefficients[[2]],digits=2),"x +",round(reg$coefficients[[1]])),"y = x"),lty=c(1,5),col=c("red","black"),bty="n",cex=0.8)
+compare_speeds <- function(wp3){
 
-plot(wp3$FP_Speed, wp3$HM_Speed,
-        xlab="FP Speed (km/s)", ylab="HM Speed (km/s)",main="WP3 CME Speeds",
-        xlim=c(0,5000), ylim=c(0,5000),
-        pch=3)
-abline(0,1,col="black",lty=5)
-reg <- lm(wp3$HM_Speed~wp3$FP_Speed)
-abline(reg,col="red")
-legend("bottomright",c(paste("y =",signif(reg$coefficients[[2]],digits=2),"x +",round(reg$coefficients[[1]])),"y = x"),lty=c(1,5),col=c("red","black"),bty="n",cex=0.8)
+	#dev.copy("CME_speeds.png",width=8,height=8,unit="in",res=300)
+	pdf("Compare_CME_Speeds.pdf",width=8,height=3)
+	par(mfrow=c(1,3),pty="s")
+	plot(wp3$FP_Speed, wp3$SSE_Speed, 
+		xlab="FP Speed (km/s)", ylab="SSE Speed (km/s)",#main="WP3 CME Speeds",
+		xlim=c(0,3000), ylim=c(0,3000),
+		pch=3)
+	abline(0,1,col="black",lty=5)
+	reg <- lm(wp3$SSE_Speed~wp3$FP_Speed)
+	abline(reg,col="red")
+	legend("bottomright",c(paste("y =",signif(reg$coefficients[[2]],digits=2),"x +",round(reg$coefficients[[1]])),"y = x"),lty=c(1,5),col=c("red","black"),bty="n",cex=0.8)
+	
+	plot(wp3$FP_Speed, wp3$HM_Speed,
+	        xlab="FP Speed (km/s)", ylab="HM Speed (km/s)",main="WP3 CME Speeds",
+	        xlim=c(0,5000), ylim=c(0,5000),
+	        pch=3)
+	abline(0,1,col="black",lty=5)
+	reg <- lm(wp3$HM_Speed~wp3$FP_Speed)
+	abline(reg,col="red")
+	legend("bottomright",c(paste("y =",signif(reg$coefficients[[2]],digits=2),"x +",round(reg$coefficients[[1]])),"y = x"),lty=c(1,5),col=c("red","black"),bty="n",cex=0.8)
+	
+	plot(wp3$HM_Speed, wp3$SSE_Speed,
+	        xlab="HM Speed (km/s)", ylab="SSE Speed (km/s)",#main="WP3 CME Speeds",
+	        xlim=c(0,5000), ylim=c(0,5000),
+	        pch=3)
+	abline(0,1,col="black",lty=5)
+	reg <- lm(wp3$SSE_Speed~wp3$HM_Speed)
+	abline(reg,col="red")
+	legend("bottomright",c(paste("y =",signif(reg$coefficients[[2]],digits=2),"x +",round(reg$coefficients[[1]])),"y = x"),lty=c(1,5),col=c("red","black"),bty="n",cex=0.8)
+	
+	dev.off()
+}
 
-plot(wp3$HM_Speed, wp3$SSE_Speed,
-        xlab="HM Speed (km/s)", ylab="SSE Speed (km/s)",#main="WP3 CME Speeds",
-        xlim=c(0,5000), ylim=c(0,5000),
-        pch=3)
-abline(0,1,col="black",lty=5)
-reg <- lm(wp3$SSE_Speed~wp3$HM_Speed)
-abline(reg,col="red")
-legend("bottomright",c(paste("y =",signif(reg$coefficients[[2]],digits=2),"x +",round(reg$coefficients[[1]])),"y = x"),lty=c(1,5),col=c("red","black"),bty="n",cex=0.8)
+#compare_speeds(wp3)
 
+# Plot out stats on the speeds
 
-dev.off()
+speed_boxplots <- function(wp3){
 
+	pdf("CME_Speeds_boxplot.pdf",width=8,height=8)
+	boxplot(wp3$FP_Speed, wp3$SSE_Speed, wp3$HM_Speed,
+                main = "WP3 CME Speeds",
+                xlab = "Geometrical Fitting Model",
+                ylab = "Speed (km/s)",
+                ylim = c(0,1500),
+		axes = F
+		)
+        axis(1, at=1:3, labels=c("Fixed-Phi","Self-Similar Exp.","Harmonic Mean"))
+	axis(2, at=seq(0,1500,by=500), las=1)
+	axis(2, at=seq(0,1500,by=100), labels=F, tcl=-0.2)
+	
+	# Ignore the horizontal version of the boxplot
+	if (FALSE) {
+	boxplot(wp3$FP_Speed, wp3$SSE_Speed, wp3$HM_Speed,
+                main = "WP3 CME Speeds",
+                ylab = "Geometrical Fitting Model",
+                xlab = "Speed (km/s)",
+                ylim = c(0,1500),
+                axes = F,
+		horizontal = TRUE
+                )
+        axis(2, at=1:3, labels=c("Fixed-Phi","Self-Similar Exp.","Harmonic Mean"))
+        axis(1, at=seq(0,1500,by=500), las=1)
+        axis(1, at=seq(0,1500,by=100), labels=F, tcl=-0.2)
+	}
 
+	dev.off()
+
+}
+
+speed_boxplots(wp3)
+
+speed_hist <- function(speed,tit){
+
+	pdf(paste("CME_Speeds_hist",tit,".pdf"),width=8,height=8)
+	
+	layout(matrix(seq(2)),heights=c(0.7,0.3))
+	par(mar=c(0,4.1,4.1,2.1)) 
+	hist(speed,breaks=100,axes=F,main=tit,xlab="",ylab="")
+	axis(2)
+	par(mar=c(5.1,4.1,0,2.1),mgp=c(3,0.5,0.0))
+	boxplot(speed,horizontal=TRUE,axes=FALSE)
+	axis(1,at=seq(0,5000,by=500),xpd=TRUE)
+	mtext("Speed (km/s)",side=1,line=2.5,font=2)
+
+	dev.off()
+
+}
+
+speed_hist(wp3$FP_Speed,"Fixed-Phi")
+speed_hist(wp3$SSE_Speed,"Self-Similar Expansion")
+speed_hist(wp3$HM_Speed,"Harmonic Mean")
